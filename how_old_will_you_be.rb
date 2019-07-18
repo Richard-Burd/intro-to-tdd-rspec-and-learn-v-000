900595242fae1835bf2455957a56e231b14648a8
Richard Burd <richardburdx@yahoo.com>
2019-07-18 18:00:56 +0000

    Automatically backed up by Learn

--- how_old_will_you_be.rb
+++ how_old_will_you_be.rb
@@ -1,5 +1,6 @@
 # Burd created this file
 
+# This section is the same as how_old_are_you.rb
 require_relative './current_age_for_birth_year.rb'
 
 puts "What year were you born?"
@@ -9,6 +10,8 @@ users_age = current_age_for_birth_year(birth_year)
 
 puts "You are: " + users_age.to_s + " years old."
 
+
+# This section was added by Burd to play around with writing Rspec tests.
 puts "Enter a year in the future to find out how old you will be in it."
 future_year = gets.to_i
 
