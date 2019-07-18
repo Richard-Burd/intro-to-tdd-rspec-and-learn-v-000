84bacccbc501dd7c89edae11dbbdda621ae91c76
Richard Burd <richardburdx@yahoo.com>
2019-07-18 18:23:02 +0000

    Automatically backed up by Learn

--- spec/how_old_will_you_be.rb
+++ spec/how_old_will_you_be.rb
@@ -4,5 +4,7 @@ require_relative '../future_age.rb'
 
 describe "future_age method" do
   it "returns the future age of a person based on the year of birth and the future year" do
+    future_year = 2050
+    birth_year = 1975
   end
 end
