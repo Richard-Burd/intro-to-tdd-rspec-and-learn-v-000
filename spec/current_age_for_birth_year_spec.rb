07ba06be7524bd4efb811238fb0e9a1a815870f4
Richard Burd <richardburdx@yahoo.com>
2019-07-18 18:27:49 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -10,16 +10,3 @@ describe "current_age_for_birth_year method" do
     expect(age_of_person).to eq(answer)
   end
 end
-
-require_relative '../future_age.rb'
-
-describe "future_age method" do
-  it "returns the future age of a person based on the year of birth and the future year" do
-    future_year = 2050
-    birth_year = 1975
-    answer = future_year - birth_year
-
-    future_age_of_person = future_age(future_year, birth_year)
-    expect(future_age_of_person).to eq(answer)
-  end
-end
