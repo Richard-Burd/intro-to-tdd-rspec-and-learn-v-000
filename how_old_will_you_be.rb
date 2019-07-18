94835a8421fd7a8104cf185869e67e59b61e478e
Richard Burd <richardburdx@yahoo.com>
2019-07-18 18:18:15 +0000

    Automatically backed up by Learn

--- how_old_will_you_be.rb
+++ how_old_will_you_be.rb
@@ -16,4 +16,6 @@ puts "You are: " + users_age.to_s + " years old."
 puts "Enter a year in the future to find out how old you will be in it."
 future_year = gets.to_i
 
-puts "In " + future_year.to_s + " you will be " + future_age.to_s
+user_future_age = future_age(future_year, birth_year)
+
+puts "In " + future_year.to_s + " you will be " + user_future_age.to_s
