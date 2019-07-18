b4d4f120e8be2848c67ad52eddde0de176085551
Richard Burd <richardburdx@yahoo.com>
2019-07-18 17:59:41 +0000

    Automatically backed up by Learn

--- how_old_will_you_be.rb
+++ how_old_will_you_be.rb
@@ -1,1 +1,15 @@
 # Burd created this file
+
+require_relative './current_age_for_birth_year.rb'
+
+puts "What year were you born?"
+birth_year = gets.to_i
+
+users_age = current_age_for_birth_year(birth_year)
+
+puts "You are: " + users_age.to_s + " years old."
+
+puts "Enter a year in the future to find out how old you will be in it."
+future_year = gets.to_i
+
+puts "In " + future_year.to_s + " You will be " + future_users_age.to_s 
