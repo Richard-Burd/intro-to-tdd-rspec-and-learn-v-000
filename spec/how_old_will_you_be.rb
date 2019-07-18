dbd767a988c44d50bbb1885b61e47d8967aa302a
Richard Burd <richardburdx@yahoo.com>
2019-07-18 17:49:00 +0000

    Automatically backed up by Learn

--- spec/how_old_will_you_be.rb
+++ spec/how_old_will_you_be.rb
@@ -1,14 +1,1 @@
 # Burd created this file
-
-require_relative '../how_old_are_you.rb'
-
-describe "current_age_for_birth_year method" do
-  it "returns the age of a person based on the year of birth" do
-    current_year = Time.now.year
-    birth_year = 1984
-    answer = current_year - birth_year
-
-    age_of_person = current_age_for_birth_year(birth_year)
-    expect(age_of_person).to eq(answer)
-  end
-end
